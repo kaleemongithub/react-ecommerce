ad0de3b92f1b86409c085227ea354c73eb79a228
Abdul Kaleem <twinklingstar.mk@gmail.com>
2024-03-10 18:15:54 +0530

    Part-2 Editing Routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../features/Navbar/Navbar"
+import ProductList from "../features/product-list/ProductList"
+const Home = () => {
+  return (
+    <div><Navbar><ProductList></ProductList></Navbar></div>
+  )
+}
+
+export default Home
